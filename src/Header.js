59d1a01afc746c55dee562df6b8d68df1bf5baf5
JacobMason83 <jmason062083@gmail.com>
2021-04-15 15:42:50 -0400

    built out the navbar, and styled the navbar as well

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import './Header.css';
 import { Link } from 'react-router-dom';
 import SearchIcon from '@material-ui/icons/Search'
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'
 function Header() {
     return (
         <nav className='header'>
@@ -14,8 +15,41 @@ function Header() {
             <input className='header__searchInput' type="text" name="" />
             <SearchIcon className="header__searchIcon" />
             </div>
-           {/* 3 links  */}
+           <div className="header__nav">
+           {/* 3 links  Link is a spa single page application that doesnt cause refreshing */}
+           {/* 1st link   */}
+           <Link to='/login' className='header__link'>
+           <div className="header__option">
+            <span className='header__optionLineOne'>Hello Jacob</span>
+            <span className='header__optionLineTwo'>Sign In </span>
+           </div>
+           </Link>
+           <Link to='/' className='header__link'>
+           {/* 2nd link   */}
+           <div className="header__option">
+            <span className='header__optionLineOne' >Returns&</span>
+            <span className='header__optionLineTwo'>Orders </span>
+           </div>
+           </Link>
+           {/* 3rd link   */}
+           <Link to='/' className='header__link'>
+           <div className="header__option">
+            <span className='header__optionLineOne'>Your</span>
+            <span className='header__optionLineTwo'>Prime </span>
+           </div>
+           </Link>
+           {/* 4th link   */}
+           <Link to='/checkout' className='header__link'>
+           <div className="header__optionBasket">
            {/* basket icon with number  */}
+               {/* shopping basket icon` */}
+                <ShoppingBasketIcon />
+               {/* number of items in the basket   */}
+               <span className='header__optionLineTwo header__basketCount'>0</span>
+           </div>
+
+           </Link>
+           </div>
         </nav>
     )
 }
